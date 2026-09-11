e1a6c33dd1b59a3cd3750046a9f37109afbb864a
fekrysaadallah <fekrysaadallah@gmail.com>
2026-09-12 02:08:07 +0300

    Solve Reverse Words in a String

---
+++ 01-strings/06-reverse-words-in-a-string.ts
@@ -0,0 +1,10 @@
+/**
+ * 151. Reverse Words in a String
+ * Link: https://leetcode.com/reverse-words-in-a-string/
+ * Time: O(n)
+ * Space: O(n)
+ */
+
+function reverseWords(s: string): string {
+  return s.split(' ').filter(Boolean).reverse().join(' ');
+}
