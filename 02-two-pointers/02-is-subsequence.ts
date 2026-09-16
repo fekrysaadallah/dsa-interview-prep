d04eb7e6cd625dd324fc5c20edd6be03708cb29f
fekrysaadallah <fekrysaadallah@gmail.com>
2026-09-16 08:28:10 +0300

    Solve Is Subsequence

---
+++ 02-two-pointers/02-is-subsequence.ts
@@ -0,0 +1,19 @@
+/**
+ * 392. Is Subsequence
+ * Link: https://leetcode.com/problems/is-subsequence/
+ * Time: O(s + t)
+ * Space: O(1)
+ */
+
+function isSubsequence(s: string, t: string): boolean {
+  let sIndex = 0;
+  let tIndex = 0;
+
+  while (sIndex < s.length && tIndex < t.length) {
+    if (s[sIndex] === t[tIndex]) sIndex++;
+
+    tIndex++;
+  }
+
+  return sIndex === s.length;
+}
